37e79ebd114389505b6c1f0c6ec97c7b4c7852c6
woutervanrijswijk <woutvr@kabelfoon.net>
2017-11-18 15:18:27 +0100

    Added IQuestion interface

---
+++ ConsoleApp7/IQuestion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp7
+{
+    interface IQuestion
+    {
+        void Display();
+        Boolean CheckAnswer(String response);
+    }
+}
